5cc57364837980ced1bf44b0cf21583b0a58e315
murali <muraligit12@gmail.com>
2024-09-10 03:24:37 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u muraliss -p dckr_pat_a4qxkW-UVOvFj-y2SGhWRT8_nls
-    docker tag web:v1 muraliss/prod_repo
-    docker push muraliss/prod_repo
+    docker tag web:v1 muraliss/dev_repo
+    docker push muraliss/dev_repo
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u muraliss -p dckr_pat_a4qxkW-UVOvFj-y2SGhWRT8_nls
-    docker tag web:v1 muraliss/dev_repo 
-    docker push muraliss/dev_repo
+    docker tag web:v1 muraliss/prod_repo 
+    docker push muraliss/prod_repo
 fi
