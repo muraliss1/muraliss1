65ee9dd0c12b9a4902274b67911320c2fc6692a8
murali <muraligit12@gmail.com>
2024-09-17 02:25:30 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $GIT_BRANCH == "dev" ]; then
+if [ $GIT_BRANCH = "dev" ]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
@@ -7,7 +7,7 @@ if [ $GIT_BRANCH == "dev" ]; then
     docker tag web:v1 muraliss/dev_repo
     docker push muraliss/dev_repo
 
-elif [ $GIT_BRANCH == "main" ]; then
+elif [ $GIT_BRANCH = "main" ]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u muraliss -p dckr_pat_a4qxkW-UVOvFj-y2SGhWRT8_nls
